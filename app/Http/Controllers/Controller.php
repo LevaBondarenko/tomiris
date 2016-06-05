f41439345008169b0594492906c7131b533c9ef6
Лев <lion.bond93@gmail.com>
2016-06-05 20:15:48 +0600

    go

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\News;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -11,4 +12,14 @@ use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 class Controller extends BaseController
 {
     use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+
+    public function LatestNews()
+    {
+
+        $news = News::orderBy('id','desc')
+            ->take(3)
+            ->get();
+
+        return \Illuminate\Support\Facades\View::make('welcome')->with('news',$news);
+    }
 }
